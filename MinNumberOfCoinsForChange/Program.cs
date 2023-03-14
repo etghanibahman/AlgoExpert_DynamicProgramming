79b379d1576297d1a0828a4d98391100447bdf95
etghanibahman <51031083+etghanibahman@users.noreply.github.com>
2023-03-14 21:51:26 +0100

    Adding Min Number of Coins

---
+++ MinNumberOfCoinsForChange/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MinNumberOfCoinsForChange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] denoms = new int[] { 1, 5, 10 };
+            int n = 7;
+            Console.WriteLine($"MinNumber Of Coins For Change : {MinNumberOfCoinsForChange(n, denoms)}");
+        }
+
+        public static int MinNumberOfCoinsForChange(int n, int[] denoms)
+        {
+            // Write your code here.
+            return -1;
+        }
+    }
+}
