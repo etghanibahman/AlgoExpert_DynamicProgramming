f8fc97582071790a96330131243a2022dae05f42
etghanibahman <51031083+etghanibahman@users.noreply.github.com>
2023-03-17 00:48:02 +0100

    Min number of coins, completed

--- MinNumberOfCoinsForChange/Program.cs
+++ MinNumberOfCoinsForChange/Program.cs
@@ -8,14 +8,28 @@ namespace MinNumberOfCoinsForChange
         {
             int[] denoms = new int[] { 1, 5, 10 };
             int n = 7;
+
+            #region Test case 5
+            denoms = new int[] { 2, 1 };
+            n = 3;
+            #endregion
             Console.WriteLine($"MinNumber Of Coins For Change : { MinNumberOfCoinsForChange(n, denoms) }");
         }
 
         public static int MinNumberOfCoinsForChange(int n, int[] denoms)
         {
-            int number = -1;
+            int[] minWays = new int[n + 1];
+            Array.Fill(minWays, Int32.MaxValue );
+            minWays[0] = 0;
+            foreach (int coin in denoms)
+            {
+                for (int i = coin; i <= n; ++i)
+                {
+                    minWays[i] = Math.Min(minWays[i],minWays[i - coin] + 1);
+                }
+            }
 
-            return number;
+            return minWays[n] == int.MaxValue  ? -1 : minWays[n];
         }
     }
 }
